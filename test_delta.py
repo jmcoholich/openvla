26e941e9389c4eebbee641f037ac6673680b3a44
Jeremiah <jcoholich@gatech.edu>
2024-08-30 14:16:21 -0400

    tester setup for controlling robot with openVLA outputs

---
+++ test_delta.py
@@ -0,0 +1,44 @@
+import torch
+import numpy as np
+from time import sleep
+
+from deoxys.franka_interface import FrankaInterface
+from deoxys.utils.config_utils import get_default_controller_config
+from deoxys_control.osc_control import move_to_target_pose, reset_joints_to  # borrowing this to move based on deltas
+from deoxys.utils.log_utils import get_deoxys_example_logger
+
+
+logger = get_deoxys_example_logger()  # logger for debugging
+
+robot_interface = FrankaInterface("/home/ripl/deoxys_control/deoxys/config/charmander.yml", use_visualizer=False)  # hardcoded path to config file, probably should change
+controller_type = "OSC_POSE"  # controls end effector in 6 dimensions, need to use serpeate controller for gripper
+controller_cfg = get_default_controller_config(controller_type=controller_type)
+
+# Golden resetting joints
+reset_joint_positions = [
+        0.09162008114028396,
+        -0.19826458111314524,
+        -0.01990020486871322,
+        -2.4732269941140346,
+        -0.01307073642274261,
+        2.30396583422025,
+        0.8480939705504309,
+    ]
+
+# reset joints to home position
+reset_joints_to(robot_interface, reset_joint_positions)
+
+
+# deltas = [x, y, z, roll, pitch, yaw, gripper]
+deltas = [0.1, 0, 0, 0, 0, 0, 0]
+
+move_to_target_pose(
+        robot_interface,
+        controller_type,
+        controller_cfg,
+        target_delta_pose=deltas[:6],
+        num_steps=80,
+        num_additional_steps=40,
+        interpolation_method="linear",
+    )
+robot_interface.close()

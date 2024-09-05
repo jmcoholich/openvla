e24d64db4268c6326ac87e9edb8ce5e9fbd945fc
Jeremiah <jcoholich@gatech.edu>
2024-09-05 14:56:45 -0400

    connected to processes stream

--- test_camera.py
+++ test_camera.py
@@ -5,33 +5,40 @@ import cv2
 from PIL import Image
 import pyrealsense2 as rs
 import numpy as np
+from openteach.utils.network import ZMQCameraSubscriber
 
 
 # Configure depth and color streams
-pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.color, 640, 480, rs.format.rgb8, 30)
+# pipeline = rs.pipeline()
+# config = rs.config()
+# config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 # Start streaming
-pipeline.start(config)
+# pipeline.start(config)
+
+image_subscriber = ZMQCameraSubscriber(
+            host = "143.215.128.151",
+            port = "10007",
+            topic_type = 'RGB'
+        )
 
 try:
     while True:
         # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-        color_frame = frames.get_color_frame()
-
-        if not color_frame:
+        frames = image_subscriber.recv_rgb_image()
+        # breakpoint()
+        color_frame = frames[0]
+        # breakpoint()
+        if color_frame is None:
             continue
 
         # Convert the frame to PIL Image
-        # depth = frames.get_depth_frame()
-        # depth_data = depth.as_frame().get_data()
-        # np_image = np.asanyarray(depth_data)
-
-        image: Image.Image = Image.fromarray(np.asanyarray(color_frame.as_frame().get_data()))
+        # img = np.asanyarray(color_frame.as_frame().get_data())
+        # img = cv2.resize(img, (224, 224))
+        image: Image.Image = Image.fromarray(color_frame)
         # Display the image
-        # cv2.imshow("Camera", image)
+        cv2.imshow("Camera", color_frame)
+        # breakpoint()
 
         # Press 'q' to exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -39,7 +46,7 @@ try:
 
 finally:
     # Stop streaming
-    pipeline.stop()
+    # pipeline.stop()
     cv2.destroyAllWindows()
 
 

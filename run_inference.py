dc02519f4db408498c380df2cc1d4c4d198467a8
Jeremiah <jcoholich@gatech.edu>
2024-10-08 17:35:54 -0400

    run inference with a finetuned model

--- run_inference.py
+++ run_inference.py
@@ -23,6 +23,8 @@ import torch
 import numpy as np
 from time import sleep
 import matplotlib.pyplot as plt
+import os
+import json
 
 
 def main():
@@ -42,24 +44,35 @@ def main():
             2.30396583422025,
             0.8480939705504309,
         ]
+
+    easy_start = [ 0.04542551, 0.41048467, 0.0139709, -1.83497724, -0.14513091, 2.2443767, 1.06756333]
     reset_joints_to(robot_interface, reset_joint_positions)  # reset joints to home position
     reset_gripper(robot_interface, logger)  # reset gripper to open
 
 
     # Load Processor & VLA
-    processor = AutoProcessor.from_pretrained("openvla/openvla-7b", trust_remote_code=True)
+    model_path = "/home/ripl/openvla/runs/openvla-7b+franka_pick_coke+b8+lr-2e-05+lora-r32+dropout-0.0+360x360"
+    vla_path = "openvla/openvla-7b"
+    processor = AutoProcessor.from_pretrained(model_path, trust_remote_code=True)
     model = AutoModelForVision2Seq.from_pretrained(
-        "openvla/openvla-7b",
+        model_path,
         torch_dtype=torch.bfloat16,
         device_map="auto",  # should use all available GPUs
         low_cpu_mem_usage=True,
         trust_remote_code=True
     )
+    dataset_statistics_path = os.path.join(model_path, "dataset_statistics.json")
+    with open(dataset_statistics_path, "r") as f:
+        norm_stats = json.load(f)
+    model.norm_stats = norm_stats
+    # breakpoint()
+
+    unnorm_key = "franka_pick_coke"
     # unnorm_key = "dlr_edan_shared_control_converted_externally_to_rlds"  # cam near base angled towards table
     # unnorm_key = "nyu_franka_play_dataset_converted_externally_to_rlds"  # franka robot cam angled away from base
-    unnorm_key = "stanford_hydra_dataset_converted_externally_to_rlds"  # franka robot cam angled towards base
+    # unnorm_key = "stanford_hydra_dataset_converted_externally_to_rlds"  # franka robot cam angled towards base
     # unnorm_key = "utaustin_mutex"  # franka robot cam straight on: this gives much bigger values than other keys so the robot moves fast
-    task_label = "move coke can near taylor swift"  # task to perform
+    task_label = "pick up the coke can"  # task to perform
     prompt = f"In: What action should the robot take to {task_label} ?\nOut:"
 
     # Configure Camera Stream
@@ -82,11 +95,12 @@ def main():
                 continue
 
             # breakpoint()
-            # color_frame = color_frame[68:292, 208:432]  # center crop 224x224
-            color_frame = cv2.resize(color_frame[:, 140:500], (224, 224))  # make the image square then scale to 224x224
+            color_frame = color_frame[:, 140:500]  # center crop 360x360
+            # color_frame = color_frame[:, :, ::-1]  # BGR to RGB
+            # color_frame = cv2.resize(color_frame[:, 140:500], (224, 224))  # make the image square then scale to 224x224
 
             # Convert the frame to PIL Image
-            image: Image.Image = Image.fromarray(color_frame)
+            # image: Image.Image = Image.fromarray(color_frame)
 
             observation = {
                         "full_image": color_frame,
@@ -106,8 +120,9 @@ def main():
                 observation,
                 task_label,
                 unnorm_key,
-                # center_crop=True
+                center_crop=True
             )
+            # action[:-1] = [i*10 for i in action[:-1]]
 
             # # Predict Action (7-DoF; un-normalize)
             # inputs = processor(prompt, image).to("cuda:0", dtype=torch.bfloat16)
@@ -119,7 +134,7 @@ def main():
                 controller_type,
                 controller_cfg,
                 target_delta_pose=action[:6],
-                num_steps=5,
+                num_steps=1,
                 num_additional_steps=0,
                 interpolation_method="linear",
                 )
@@ -131,10 +146,10 @@ def main():
                 break
 
             # graph action values on a line graph
-            if summary is None:
-                summary = action
-            else:
-                summary = np.vstack((summary, action))
+            # if summary is None:
+            #     summary = action
+            # else:
+            #     summary = np.vstack((summary, action))
 
     finally:
         cv2.destroyAllWindows()
